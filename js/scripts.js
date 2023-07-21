ab0a737394943db67e652b477669719cfec7472b
Alex <alexanaya@protonmail.com>
2023-07-21 17:24:02 -0500

    knowledgbase page fix + setting up js

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,46 @@ $(function () {
         }
     });
 })
+
+
+//making knowledgebase page layout responsive
+$(function () {
+    let viewport = window.matchMedia("(min-width: 576px)");
+    if (viewport.matches) {
+        $(".plantEntryInfo").addClass("d-none");
+    }
+    $(window).on('resize', function () {
+        if ($(window).width() >= 576) {
+            $(".plantEntryInfo").addClass("d-none");
+        } else if ($(window).width() < 576) {
+            $(".plantEntryInfo").removeClass("d-none");
+        }
+    });
+})
+
+
+
+
+
+//populating knowledgebase
+
+
+class PlantEntry {
+    constructor(plantName, classification, description, tags) {
+    this.plantName = plantName;
+    this.classification = classification;
+    this.description = description;
+    this.tags = tags;
+    }
+}
+
+class Encounter extends PlantEntry {
+    constructor(plantName, classification, description, tags, author, date, entryText) {
+        super(plantName, classification, description, tags);
+        this.author = author;
+        this.date = date;
+        this.entryText = entryText;
+    }
+}
+
+
